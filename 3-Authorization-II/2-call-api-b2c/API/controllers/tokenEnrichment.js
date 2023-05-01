b94247bd611cc1b88db9696cacff6e87da600a31
Yan <yanc@ext.inditex.com>
2023-05-01 17:37:38 +0300

    rr

--- 3-Authorization-II/2-call-api-b2c/API/controllers/tokenEnrichment.js
+++ 3-Authorization-II/2-call-api-b2c/API/controllers/tokenEnrichment.js
@@ -2,12 +2,16 @@ exports.enrichToken = (req, res, next) => {
 
     const claims = req.body;
 
-    res.send({
+    const r = {
         "version": "1.0.0",
         "action": "Continue",
         "postalCode": claims.email,
-        "extension_cb90c9c3-f2a8-40ac-b049-a35b687c1575_role": "idrole" ,
-        "extension_roles": "roles",
+        "extension_roles": "admin, agent",
         "extension_customroles": "customroles"
-    });
+    }
+
+    console.log('req', claims);
+    console.log('res', r);
+
+    res.send(r);
 };
