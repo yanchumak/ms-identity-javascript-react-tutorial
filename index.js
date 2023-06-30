9d44dab6ec3970227b2db234ac5678e946e4a130
Yan <yanc@ext.inditex.com>
2023-06-30 13:36:29 +0300

    rr

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require("http");
 
-const host = 'localhost';
 const port = 8080;//
 
 const requestListener = function (req, res) {
@@ -16,6 +15,6 @@ const requestListener = function (req, res) {
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
