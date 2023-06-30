9772b9b3580f78b48bdfbe3e8af34e5ecd0afd03
Yan <yanc@ext.inditex.com>
2023-06-30 13:12:04 +0300

    rr

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const host = 'localhost';
-const port = 80;
+const port = 8080;
 
 const requestListener = function (req, res) {
     res.setHeader("Content-Type", "application/json");
