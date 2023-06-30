a11f01b7e90f78fe40cd4d065c30930d0a7f3ab8
Yan <yanc@ext.inditex.com>
2023-06-30 12:15:51 +0300

    rr

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    const data = {
+        now: new Date(),
+        url: req.url,
+        method: req.method,
+        rawHeaders: req.rawHeaders
+    };
+    res.end(JSON.stringify(data));
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
